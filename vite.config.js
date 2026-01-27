7e3c1f22737f3b7680c246a1931d615e554edb4b
Jeri <124492860+gg123Mvc@users.noreply.github.com>
2026-01-28 06:17:29 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
 export default defineConfig({
-  base: "/Puyuh/",   // ❗ ini wajib
-  plugins: [react()],
+  base: "/",
+  plugins: [react()]
 });
