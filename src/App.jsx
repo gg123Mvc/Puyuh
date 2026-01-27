bd250091d818ba2458511b14d3f4d15f8a63b162
gg123Mvc <Muhamadjeri7@gmail.com>
2026-01-28 06:48:17 +0700

    git commit -m "update project"

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./Layout"
-import Home from "./Home"
+import Home from "./Home"; // path BENAR
 import Profile from "./Profile"
 import Services from "./Services"
 
