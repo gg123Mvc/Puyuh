6b33ccfb4307fc77ae68280afb65f0a7307687fe
gg123Mvc <Muhamadjeri7@gmail.com>
2026-01-28 07:28:34 +0700

    baru lagi

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Hapus jika tidak pakai React
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Opsional: Naikkan batas peringatan
+  },
 });
