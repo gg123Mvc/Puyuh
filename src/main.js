e885e5d35b30e3bd09e109b643522b0ad4978953
Jeri <124492860+gg123Mvc@users.noreply.github.com>
2026-01-28 10:43:35 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>TEST REACT JALAN</h1>
-);
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
