0bee79ca94e46012f00851e3ae5af5d736f45259
Jeri <124492860+gg123Mvc@users.noreply.github.com>
2026-01-28 07:32:55 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-import react from '@vitejs/plugin-react'; // Hapus jika tidak pakai React
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1000, // Opsional: Naikkan batas peringatan
-  },
 });
