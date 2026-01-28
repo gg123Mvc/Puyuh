4d6418e4be9b50099307f8a57eca88ed264e2407
gg123Mvc <Muhamadjeri7@gmail.com>
2026-01-28 09:58:49 +0700

    bgttt

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: 'jsx',
+    include: /src\/.*\.jsx?$/,
+    exclude: [],
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   base: '/puyuh',
 })
